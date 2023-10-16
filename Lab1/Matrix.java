b0cace79a8423f869add9cb52653a54831476b9c
Khanh <khanhc2k31@gmail.com>
2023-10-16 18:52:31 +0700

    Bai 10

---
+++ Lab1/Matrix.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class Matrix{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+
+        System.out.println("Enter the elements of the first matrix:");
+        enterMatrixElements(matrix1, scanner);
+
+        System.out.println("Enter the elements of the second matrix:");
+        enterMatrixElements(matrix2, scanner);
+
+        int[][] resultMatrix = addMatrices(matrix1, matrix2);
+
+    
+        System.out.println("Resultant Matrix:");
+        printMatrix(resultMatrix);
+
+        scanner.close();
+    }
+
+    public static void enterMatrixElements(int[][] matrix, Scanner scanner) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
+        int rows = matrix1.length;
+        int cols = matrix1[0].length;
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
