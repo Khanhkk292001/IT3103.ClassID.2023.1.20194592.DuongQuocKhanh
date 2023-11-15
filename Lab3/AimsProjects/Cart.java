1332b1003859de73e7cd74275a0c9e95561788ba
Khanh <khanhc2k31@gmail.com>
2023-11-15 16:33:46 +0700

    Update Cart.java

--- Lab3/AimsProjects/Cart.java
+++ Lab3/AimsProjects/Cart.java
@@ -6,7 +6,7 @@ public class Cart {
     private DigitalVideoDisc[] itemsInCart = new DigitalVideoDisc[MAX_CART_SIZE];
     private int cartSize = 0;
 
-    //  Phương thức hiện tại để thêm một DVD vào giỏ hàng
+    // Phương thức hiện tại để thêm một DVD vào giỏ hàng
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (cartSize >= MAX_CART_SIZE - 1) {
             System.out.println("Cart is FULL. Cannot add more DVDs!");
@@ -16,29 +16,35 @@ public class Cart {
         }
     }
 
-    // Phương thức được nạp chồng để thêm một danh sách các DVD vào giỏ hàng (sử dụng mảng tham số)
+        // Phương thức được nạp chồng để thêm một danh sách các DVD vào giỏ hàng (sử dụng mảng tham số)
     public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
         for (DigitalVideoDisc dvd : dvdList) {
             addDigitalVideoDisc(dvd);
         }
     }
 
-    // Phương thức được nạp chồng để thêm một số lượng tùy ý DVD vào giỏ hàng (sử dụng varargs)
-  /*  public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
-        for (DigitalVideoDisc dvd : dvds) {
-            addDigitalVideoDisc(dvd);
-        }
-    }    */
-
-  
-    // Phương thức được nạp chồng để thêm hai DVD vào giỏ hàng
+     // Phương thức được nạp chồng để thêm hai DVD vào giỏ hàng
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
         addDigitalVideoDisc(dvd1);
         addDigitalVideoDisc(dvd2);
     }
 
-    // Tính tổng các DVD đã thêm vào giỏ
+    // In ra mục lục
+    public void printOrder() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:\n");
 
+        for (int i = 0; i < cartSize; i++) {
+            DigitalVideoDisc dvd = itemsInCart[i];
+            System.out.println(i + 1 + ". DVD - " + "[" + dvd.getTitle() + "] - [" + dvd.getCategory() +
+                    "] - [" + dvd.getDirector() + "] - [" + dvd.getLength() + "]: " + dvd.getCost() + " $");
+        }
+
+        System.out.println("\nTotal cost: " + totalCost());
+        System.out.println("***************************************************");
+    }
+
+    // Tính tổng thêm
     public float totalCost() {
         float sum = 0;
         for (int i = 0; i < cartSize; i++) {
